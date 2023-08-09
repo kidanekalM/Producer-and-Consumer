4cf29f5a61a60fc148b265766141639f9fb286d5
Kidanekal <akidanekal@gmail.com>
2023-08-09 17:55:03 +0900

    created producer and consumer project
    added responsiveness and logic as well

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
 let buffer = []
-let max = 5
-function produce() {
-    for(i=0;i<max;i++){
-       setTimeout(function () {
+let max = 7
+let timer = Object()
+timer.consumer = 0
+timer.producer = 0
+function produce(time) {
+timer.producer =  setInterval(function () {
+    console.log("producing");
+    buffer = document.getElementById('buffer')
+    if(buffer.children.length==max){
+        document.getElementById('producer').style.backgroundColor="goldenrod"
+    }
+    else{
+        document.getElementById('producer').style.backgroundColor="rgb(191, 255, 64)"
         let job = document.createElement('div')
         job.className = "item"
         job.animationName ="produce"
-        buffer.push(job)
-        document.getElementById('buffer').appendChild(job)
-         },1000)
+        buffer.firstElementChild? buffer.firstElementChild.before(job):buffer.appendChild(job)
+    }
+        },time)
+}
+function consume(time) {
+ timer.consumer = setInterval(function () {
+    console.log("consuming");
+    console.log(buffer);
+    buffer = document.getElementById('buffer')
+        if((buffer.childElementCount>0)&& (buffer.lastElementChild)){
+            document.getElementById('consumer').style.backgroundColor="rgb(191, 255, 64)"
+            job = buffer.lastElementChild;
+            console.log(job);
+            job.style.animationName = "consume"
+            setTimeout(() => {
+                if(buffer.childElementCount>0){
+                    buffer.removeChild(job)
+                }
+            }, 2000);
+
+        }
+        else{
+            document.getElementById('consumer').style.backgroundColor="goldenrod"
+        }
+    },time,timer)
+}
+document.getElementById('start').addEventListener('click',function () {
+if(this.innerText == "Start"){
+    this.innerText = "Stop";
+    let ct = parseFloat(document.getElementById('consumerTime').value);
+    let pt = parseFloat(document.getElementById('producerTime').value);
+    if((ct < 0)|| (pt<0)){
+        alert("time cannot be less than 0")
+    }
+    else{
+        consume(ct*1000)
+        produce(pt*1000)
+
     }
 }
-function consume() {
-    while(buffer.length>0){
-       job = buffer.shift();
-       job.style.animationName = "consume"
-       document.getElementById('buffer').remove(job)
-    }
+else{
+    clearInterval(timer.consumer)
+    clearInterval(timer.producer)
+    this.innerText = "Start";
 }
-produce()
-setTimeout(function () {
-consume()
-},5000)
+})
